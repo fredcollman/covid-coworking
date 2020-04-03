15391526cbd91c5cceb06acebbbbe829cfd19d31
Fred Collman <fredcollman@gmail.com>
2020-04-03 14:11:46 +0100

    Draw a box

--- src/client.js
+++ src/client.js
@@ -1,2 +1,7 @@
 // flow
-console.log("loaded");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#f00";
+ctx.rect(180, 100, 50, 50);
+ctx.fill();
