d6f783341f91524a7f62fa61020450f63cbb352c
Fred Collman <fredcollman@gmail.com>
2020-04-03 14:43:06 +0100

    Make the rectangle move

--- src/client.js
+++ src/client.js
@@ -2,6 +2,18 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#f00";
-ctx.rect(180, 100, 50, 50);
-ctx.fill();
+const draw = (timestamp) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#f00";
+  ctx.fillRect(timestamp, Math.min(timestamp + 20, canvas.height - 50), 50, 50);
+};
+
+const loopForever = (callback) => {
+  const loop = (timestamp) => {
+    callback(timestamp);
+    window.requestAnimationFrame(loop);
+  };
+  window.requestAnimationFrame(loop);
+};
+
+loopForever(draw);
