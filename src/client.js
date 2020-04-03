3919d52c46b068d0d83b54d1a396e0bb6819c7a4
Fred Collman <fredcollman@gmail.com>
2020-04-03 14:59:19 +0100

    Move up/down/left/right

--- src/client.js
+++ src/client.js
@@ -3,8 +3,11 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const position = { x: 100, y: 100 };
+const speed = { x: 0, y: 0 };
 
 const draw = (timestamp) => {
+  position.x += speed.x;
+  position.y += speed.y;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#f00";
   ctx.fillRect(position.x, position.y, 50, 50);
@@ -22,15 +25,45 @@ const receiveInput = (handlers) => (event) => {
   switch (event.keyCode) {
     case 37:
       return handlers.left();
+    case 38:
+      return handlers.up();
     case 39:
       return handlers.right();
+    case 40:
+      return handlers.down();
   }
 };
 
-const handlers = {
-  left: () => position.x--,
-  right: () => position.x++,
+const downHandlers = {
+  left: () => {
+    speed.x = -5;
+  },
+  right: () => {
+    speed.x = 5;
+  },
+  up: () => {
+    speed.y = -5;
+  },
+  down: () => {
+    speed.y = 5;
+  },
+};
+
+const upHandlers = {
+  left: () => {
+    speed.x = 0;
+  },
+  right: () => {
+    speed.x = 0;
+  },
+  up: () => {
+    speed.y = 0;
+  },
+  down: () => {
+    speed.y = 0;
+  },
 };
 
 loopForever(draw);
-document.addEventListener("keydown", receiveInput(handlers));
+document.addEventListener("keydown", receiveInput(downHandlers));
+document.addEventListener("keyup", receiveInput(upHandlers));
