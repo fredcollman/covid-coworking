3e8565dcc253e650686f943db8352dfaa44a907f
Fred Collman <fredcollman@gmail.com>
2020-04-03 14:53:13 +0100

    Move the rectangle via keys

--- src/client.js
+++ src/client.js
@@ -2,10 +2,12 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const position = { x: 100, y: 100 };
+
 const draw = (timestamp) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#f00";
-  ctx.fillRect(timestamp, Math.min(timestamp + 20, canvas.height - 50), 50, 50);
+  ctx.fillRect(position.x, position.y, 50, 50);
 };
 
 const loopForever = (callback) => {
@@ -16,4 +18,19 @@ const loopForever = (callback) => {
   window.requestAnimationFrame(loop);
 };
 
+const receiveInput = (handlers) => (event) => {
+  switch (event.keyCode) {
+    case 37:
+      return handlers.left();
+    case 39:
+      return handlers.right();
+  }
+};
+
+const handlers = {
+  left: () => position.x--,
+  right: () => position.x++,
+};
+
 loopForever(draw);
+document.addEventListener("keydown", receiveInput(handlers));
