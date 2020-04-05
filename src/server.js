d8deca24ff356166fdf3e592cbffa7e49a01019c
Fred Collman <fredcollman@gmail.com>
2020-04-05 19:31:57 +0100

    Switch to Dokku port 5000

--- src/server.js
+++ src/server.js
@@ -25,6 +25,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on port 3000");
+server.listen(5000, () => {
+  console.log("listening on port 5000");
 });
